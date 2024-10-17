4dd778383929f368958c2b76d8a60abceda7fc4e
Jhon Francia Minaya <jfranciaminaya@gmail.com>
2024-10-17 09:42:56 -0500

    Plantilla AdminLtE

---
+++ app/config/App.php
@@ -0,0 +1,11 @@
+<?php
+
+//Constantes globales
+const SERVERURL = "http://localhost/permisos/";
+const COMPANY = "SENATI";
+const VERSION = "1.0";
+
+//Configuración horaria
+date_default_timezone_set("America/Lima");
+
+//Recursos - HELPERS...
